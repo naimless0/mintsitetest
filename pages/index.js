8cb41ac8d715d6ec537b968550131d975d1b0867
naimless0 <naimsmith2000@gmail.com>
2023-07-17 11:10:31 +0200

    Version 2

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,27 @@
 import { useContract, useContractRead, useContractWrite, Web3Button, useAddress, ConnectWallet } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
-import { ethers, utils } from "ethers";
+import { ethers } from "ethers";
 
 
-const contractAddress = "0x8F08597e28F2b4F4F153970742B1cc532C619F57";
+const contractAddress = "0xA96a6Bdf602488e8fD5a753107963034cBd27AA0";
 
 
 export default function Home() {
+
   const { contract } = useContract(contractAddress);
   const address = useAddress();
   const { data: getnumdata } = useContractRead(contract, "nextTokenIdToClaim"); 
-  const { mutateAsync: claim, isLoading } = useContractWrite(contract, "claim")
-
+  const { mutateAsync: claim, isLoading } = useContractWrite(contract, "claim");
+  const { data: priceToPass } = useContractRead(contract, "nextTokenIdToClaim");
   const call = async () => {
-    try {
-      const data = await claim({ args: [_receiver, _quantity, _currency, _pricePerToken, _allowlistProof, _data] });
-      console.info("contract call successs", data);
-    } catch (err) {
-      console.error("contract call failure", err);
-    }
+    const _price = await priceToPass / 2;
+    const data = await contract?.call(
+      "claim", // Name of your function as it is on the smart contract
+      [address, "1", "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", "500000000000000000", { "proof": ["0x0000000000000000000000000000000000000000000000000000000000000000"], "quantityLimitPerWallet": 0, "pricePerToken": 0, "currency": "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" }, "0x00"],
+      {
+        value: ethers.utils.parseEther(_price.toString()), // send token with the contract call
+      },
+    );
   }
 
   return (
@@ -29,7 +32,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Join Now!
+          Join the Nation of the Disciplined!
         </p>
         <div className={styles.connect}>
           <ConnectWallet />
@@ -41,23 +44,24 @@ export default function Home() {
               {getnumdata?.toNumber()/2} Matic
             </p>
           </a>
-
           <a className={styles.card}>
-            <h2>Mint Your NFT Card &rarr;</h2>
+            <h2>Mint Your Card!</h2>
             <p>
-               <Web3Button
-               contractAddress = "0x8F08597e28F2b4F4F153970742B1cc532C619F57"
-               action={(contract) => {
-                contract.call("claim", [address, "1", "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", "500000000000000000", { "proof": ["0x0000000000000000000000000000000000000000000000000000000000000000"], "quantityLimitPerWallet": 0, "pricePerToken": 0, "currency": "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" }, "0x00"], utils.parseEther("2") ,)
-               }
-              }
-                  onSuccess={(result) => alert("Congratulations!")}
-                >
-                 Mint
-                </Web3Button>
+              {contract && 
+              <>
+              <div>
+                <button
+
+               onClick={() => call()}
+               className={styles.buttn1}
+               >
+               Mint 
+
+              </button>
+              </div>
+              </>}
             </p>
           </a>
-
           <a
             className={styles.card}
           >
